330e5dfded09d1c783c65b8683b0032d1874b713
Ho Yin Leung <leunghoyinhk@gmail.com>
2021-02-12 18:33:19 +0000

    lesson 24 final

--- allOrders.php
+++ allOrders.php
@@ -1,3 +1,25 @@
 <?php include('header.php'); ?>
-全部訂單頁
+
+<h1>收到的訂單</h1>
+
+<?php
+//拿訪客的訂單資料
+$orderData = file_get_contents('data.csv');
+$orders = str_getcsv($orderData, "\r\n");
+
+//顯示所有訂單
+foreach($orders as $order)
+{
+    //拆解每一單的幾個資料
+    $singleOrder = explode(",", $order);
+
+    echo '<div class="order"><p>';
+    echo '客戶稱呼 : '.$singleOrder[1].'<br/>';
+    echo '客戶電郵 : '.$singleOrder[2].'<br/>';
+    echo '想預訂 : '.$gems[$singleOrder[0]-1]['name'].' X '.$singleOrder[3].'件 <br/>';
+    echo '下單時間 : '.$singleOrder[4].'<br/>';
+    echo '</p></div>';
+}
+?>
+
 <?php include('footer.php'); ?>
--- functions.php
+++ functions.php
@@ -9,14 +9,17 @@ if($_GET['op']=='logout')
 }
 
 function createOrder(){
-    echo $_POST['gem_id']."<br>";
+    /* echo $_POST['gem_id']."<br>";
     echo $_POST['name']."<br>";
     echo $_POST['email']."<br>";
     echo $_POST['quantity']."<br>";
-    echo date('Y-m-d H:i:s')."<br>";
+    echo date('Y-m-d H:i:s')."<br>"; */
 
     //儲存訂單
-
+    $myfile = fopen("data.csv", "a") or die("未能開啟檔案");
+    $data = $_POST['gem_id'].','.$_POST['name'].','.$_POST['email'].','.$_POST['quantity'].','.date('Y-m-d H:i:s')."\r\n";
+    fwrite($myfile, $data);
+    fclose($myfile);
 
     //轉變頁面
     header("Location: /order-completed.php");
