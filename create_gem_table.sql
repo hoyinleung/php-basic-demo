a6c25651746d3ad234d9288b4c6a536297e519c2
Ho Yin Leung <leunghoyinhk@gmail.com>
2021-02-19 00:21:02 +0000

    added gem_id

--- create_gem_table.sql
+++ create_gem_table.sql
@@ -13,10 +13,12 @@
 
 
 -- Dumping database structure for php_gem
+DROP DATABASE IF EXISTS `php_gem`;
 CREATE DATABASE IF NOT EXISTS `php_gem` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 USE `php_gem`;
 
 -- Dumping structure for table php_gem.gem
+DROP TABLE IF EXISTS `gem`;
 CREATE TABLE IF NOT EXISTS `gem` (
   `gem_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
@@ -29,18 +31,21 @@ CREATE TABLE IF NOT EXISTS `gem` (
 -- Data exporting was unselected.
 
 -- Dumping structure for table php_gem.order
+DROP TABLE IF EXISTS `order`;
 CREATE TABLE IF NOT EXISTS `order` (
   `order_id` int(11) NOT NULL AUTO_INCREMENT,
   `client_name` varchar(255) NOT NULL DEFAULT '0',
   `client_email` varchar(255) NOT NULL DEFAULT '0',
   `quantity` int(11) NOT NULL DEFAULT '0',
   `order_time` timestamp NOT NULL,
+  `gem_id` int(11) DEFAULT NULL,
   KEY `Index 1` (`order_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
 -- Data exporting was unselected.
 
 -- Dumping structure for table php_gem.staff
+DROP TABLE IF EXISTS `staff`;
 CREATE TABLE IF NOT EXISTS `staff` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) DEFAULT NULL,
